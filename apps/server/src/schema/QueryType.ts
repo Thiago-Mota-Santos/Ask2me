90e7afcaffa7c9ff92f803274ff3832e75d61e03
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-03-20 08:39:09 -0300

    feat(profile) return one profile

--- apps/server/src/schema/QueryType.ts
+++ apps/server/src/schema/QueryType.ts
@@ -1,10 +1,12 @@
 import { GraphQLFieldConfig, GraphQLNonNull, GraphQLObjectType } from 'graphql'
 import { nodeField, nodesField } from '../node/typeRegister'
-import { AppointmentConnection } from '../modules/appointment/AppointmentType'
+import { AppointmentConnection } from '../modules/appointment/appointmentType'
 import { connectionArgs } from '@entria/graphql-mongo-helpers'
-import { AppointmentLoader } from '../modules/appointment/AppointmentLoader'
-import { UserType } from '../modules/user/UserType'
-import { UserLoader } from '../modules/user/UserLoader'
+import { AppointmentLoader } from '../modules/appointment/appointmentLoader'
+import { UserType } from '../modules/user/userType'
+import { UserLoader } from '../modules/user/userLoader'
+import { ProfileType } from '../modules/profile/profileType'
+import { ProfileModel } from '../modules/profile/profileModel'
 
 const appointments: GraphQLFieldConfig<any, any, any> = {
   type: new GraphQLNonNull(AppointmentConnection.connectionType),
@@ -13,11 +15,27 @@ const appointments: GraphQLFieldConfig<any, any, any> = {
     await AppointmentLoader.loadAll(context, _args),
 }
 
+const profile: GraphQLFieldConfig<any, any, any> = {
+  type: ProfileType,
+  resolve: async (_root, _args, context) => {
+    
+    const userId = context.user?._id;
+    if (!userId) {
+      throw new Error('User not authenticated');
+    }
+
+    const profile = await ProfileModel.findOne({ profileId: userId });
+    console.log(profile)
+    return profile; 
+  },
+};
+
+
 const me: GraphQLFieldConfig<any, any, any> = {
   type: UserType,
   description: 'user logged',
-  resolve: async (_root, _args, context) =>
-    await UserLoader.loadAll(context, context.user?._id),
+  resolve: async (_, __, context) =>
+  await UserLoader.load(context, context.user?._id),
 }
 
 export const QueryType = new GraphQLObjectType({
@@ -26,6 +44,7 @@ export const QueryType = new GraphQLObjectType({
   fields: () => ({
     node: nodeField,
     nodes: nodesField,
+    profile,
     appointments,
     me,
   }),
