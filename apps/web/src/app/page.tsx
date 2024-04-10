f6a8cd6a0d7d294cd921cca63af0b96379c12fdb
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-04-10 12:36:27 -0300

    feat(remove) web (next13)

--- apps/web/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-red-300">hello world</h1>
-    </div>
-  );
-}
