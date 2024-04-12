d3775febf65c6dc63c2bfc0c3500b94872f16ab8
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-04-12 13:58:23 -0300

    feat(wrapper) getPreloadedQuery response

--- apps/next/src/relay/network.ts
+++ apps/next/src/relay/network.ts
@@ -1,58 +1,45 @@
-import type {
-  CacheConfig,
-  ConcreteRequest,
-  RequestParameters,
-  Variables,
-} from 'relay-runtime'
-import { Network, QueryResponseCache } from 'relay-runtime'
+import { CacheConfig, ConcreteRequest, Network, QueryResponseCache, RequestParameters, Variables } from 'relay-runtime';
 
-const ONE_MINUTE_IN_MS = 60 * 1000
+const ONE_MINUTE_IN_MS = 60 * 1000;
 
-const GRAPHQL_ENPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string
-
-export function createNetwork() {
+function createNetwork() {
   const responseCache = new QueryResponseCache({
     size: 100,
     ttl: ONE_MINUTE_IN_MS,
-  })
+  });
 
-  async function fetchResponse(
-    operation: RequestParameters,
-    variables: Variables,
-    cacheConfig: CacheConfig,
-  ) {
-    const { id } = operation
+  async function fetchResponse(operation: RequestParameters, variables: Variables, cacheConfig: CacheConfig) {
+    const { id } = operation;
 
-    const isQuery = operation.operationKind === 'query'
-    const forceFetch = cacheConfig && cacheConfig.force
+    const isQuery = operation.operationKind === 'query';
+    const forceFetch = cacheConfig && cacheConfig.force;
+
     if (isQuery && !forceFetch) {
-      const fromCache = responseCache.get(id!, variables)
+      const fromCache = responseCache.get(id!, variables);
       if (fromCache != null) {
-        return Promise.resolve(fromCache)
+        return Promise.resolve(fromCache);
       }
     }
 
-    return networkFetch(operation, variables)
+    return networkFetch(operation, variables);
   }
 
-  const network = Network.create(fetchResponse)
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore - seems to be a private untyped api
-
-  network.responseCache = responseCache
-  return network
+  const network = Network.create(fetchResponse);
+  // @ts-ignore Private API Hackery? 🤷‍♂️
+  network.responseCache = responseCache;
+  return network;
 }
 /**
  * Relay requires developers to configure a "fetch" function that tells Relay how to load
  * the results of GraphQL queries from your server (or other data source). See more at
  * https://relay.dev/docs/en/quick-start-guide#relay-environment.
  */
-async function networkFetch(
-  params: RequestParameters,
-  variables: Variables,
-  headers?: HeadersInit,
-) {
+
+const GRAPHQL_ENPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string;
+
+async function networkFetch(params: RequestParameters, variables: Variables, headers?: HeadersInit) {
   // Fetch data from GitHub's GraphQL API:
+  // TODO: Add GraphQL Endpoint
   const response = await fetch(GRAPHQL_ENPOINT, {
     method: 'POST',
     credentials: 'include',
@@ -64,28 +51,27 @@ async function networkFetch(
       query: params.text,
       variables,
     }),
-  })
+  });
 
   // Get the response as JSON
-  const json = await response.json()
+  const json = await response.json();
 
   // GraphQL returns exceptions (for example, a missing required variable) in the "errors"
   // property of the response. If any exceptions occurred when processing the request,
   // throw an error to indicate to the developer what went wrong.
   if (Array.isArray(json.errors)) {
-    console.log(json.errors)
-
     throw new Error(
-      `Error fetching GraphQL query '${
-        params.name
-      }' with variables '${JSON.stringify(variables)}': ${JSON.stringify(
+      `Error fetching GraphQL query '${params.name}' with variables '${JSON.stringify(variables)}': ${JSON.stringify(
         json.errors,
       )}`,
-    )
+    );
   }
+
+  // Otherwise, return the full payload.
+  return json;
 }
 
-export async function getPreloadedQuery(
+async function getPreloadedQuery(
   { params }: ConcreteRequest,
   variables: Variables,
   headers: HeadersInit,
@@ -102,3 +88,5 @@ export async function getPreloadedQuery(
     return null; 
   }
 }
+
+export { createNetwork, getPreloadedQuery };
