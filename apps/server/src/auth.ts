51a52c8ff61f0c58b9b5f40778e7d9aba1307ee5
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-04-16 12:03:40 -0300

    test(try) remove

--- apps/server/src/auth.ts
+++ apps/server/src/auth.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 import { ParameterizedContext } from 'koa'
 import { UserDocument, UserModel } from './modules/user/userModel'
 import { Maybe } from '../../../packages/types/src'
-import { debugConsole } from '../test/debubConsole'
 
 const JWT_KEY = process.env.JWT_KEY as string
 
@@ -11,22 +10,25 @@ const getUser = async (
   ctx: ParameterizedContext,
 ): Promise<{ user: Maybe<UserDocument> }> => {
 
-  const token = ctx.cookies.get('token')
-  try {
-    if (!token) {
-      return { user: null }
-    }
-    const subToken = token?.replace('JWT%20', '')
-    
-    const decodedToken = jwt.verify(subToken!, JWT_KEY)
-    const decodedId = decodedToken as { id: string }
-    
-    const user = await UserModel.findOne({ _id: decodedId.id })
-    return { user }
-  } catch (err) {
-    return { user: null }
+  const token = ctx.cookies.get('token');
+
+  if (!token) {
+    return { user: null };
   }
-}
+
+  const subToken = token?.replace('JWT ', '');
+
+  let decodedId;
+  let user;
+
+  const verifyTokenAndFindUser = async () => {
+    decodedId = jwt.verify(subToken!, JWT_KEY) as { id: string };
+    user = await UserModel.findOne({ _id: decodedId.id });
+  };
+
+  await verifyTokenAndFindUser().catch(() => {});
+  return { user: user };
+};
 
 const generateJwtToken = (user: UserDocument) => {
   return `JWT ${jwt.sign({ id: user._id }, JWT_KEY)}`
